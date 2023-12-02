3349944f3d219a2be7c9862cbf780db7e4fe5de9
AEP <91467886+TukangM@users.noreply.github.com>
2023-12-02 13:10:53 +0700

    Update gotty.sh

--- gotty.sh
+++ gotty.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Specify the desired options for gotty
-./gotty -p 1024 -w bash
+./gotty -p 1024 -w bash &
 ./ngrok authtoken 2IdAgMjWOHBPJZer0vYuoZc2qJY_3vdg6Qf4Ssq5dJm1Trpua
 ./ngrok tcp 1024
