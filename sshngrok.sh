e003b597d1bd51d0e85d6a382a714b39ef88bb39
AEP <91467886+TukangM@users.noreply.github.com>
2023-12-02 14:00:59 +0700

    Update sshngrok.sh

--- sshngrok.sh
+++ sshngrok.sh
@@ -7,15 +7,6 @@ apt install openssh-server sudo apt-utils -y
 # Mulai SSH Server
 sudo service ssh start
 
-# Buat pengguna serah lu sih wkwkwkw
-useradd -m ash
-
-# Set kata sandi untuk pengguna yusup (909909)
-echo "ash:112233" | sudo chpasswd
-
-# Tambahkan pengguna yusup ke grup sudo
-sudo usermod -aG sudo ash
-
 # Masukkan token authtoken Ngrok Anda di sini
 ./ngrok authtoken 2IdAgMjWOHBPJZer0vYuoZc2qJY_3vdg6Qf4Ssq5dJm1Trpua
 
