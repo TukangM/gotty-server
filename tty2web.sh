9044e12e18eb76f44ce3386223465d1a7d9acadf
AEP <91467886+TukangM@users.noreply.github.com>
2023-12-02 13:43:50 +0700

    Update tty2web.sh

--- tty2web.sh
+++ tty2web.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Specify the desired options for gotty
+# Specify the desired options for tty2web
+chmod +x tty2web
 ./tty2web --term sshburik --port 1024 bash &
 ./ngrok authtoken 2IdAgMjWOHBPJZer0vYuoZc2qJY_3vdg6Qf4Ssq5dJm1Trpua
 ./ngrok tcp 1024
