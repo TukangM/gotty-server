6146aac3c58106652c780ec469973244a6de9c67
AEP <91467886+TukangM@users.noreply.github.com>
2023-12-02 13:47:30 +0700

    Update tty2web.sh

--- tty2web.sh
+++ tty2web.sh
@@ -2,6 +2,6 @@
 
 # Specify the desired options for tty2web
 chmod +x tty2web
-./tty2web --term sshburik --port 1024 bash &
+./tty2web --port 1024 -w bash &
 ./ngrok authtoken 2IdAgMjWOHBPJZer0vYuoZc2qJY_3vdg6Qf4Ssq5dJm1Trpua
 ./ngrok tcp 1024
