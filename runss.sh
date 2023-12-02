a5fb7b730ef860f52ab2c5ff2f1d9c4d2a97509f
AEP <91467886+TukangM@users.noreply.github.com>
2023-12-02 14:01:10 +0700

    Update runss.sh

--- runss.sh
+++ runss.sh
@@ -13,15 +13,6 @@ systemctl start ssh
 apt install sudo -y
 apt install apt-utils -y
 
-# Buat pengguna yusup
-useradd -m ash
-
-# Set kata sandi untuk pengguna yusup (909909)
-echo "ash:112233" | sudo chpasswd
-
-# Tambahkan pengguna yusup ke grup sudo
-sudo usermod -aG sudo ash
-
 # Masukkan token authtoken Ngrok Anda di sini
 ./ngrok authtoken 2IdAgMjWOHBPJZer0vYuoZc2qJY_3vdg6Qf4Ssq5dJm1Trpua
 
